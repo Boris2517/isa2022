a89227ebefdf2d4512aa63558d14c69377265f84
Boris Knezevic <appsyncwork@gmail.com>
2022-10-26 00:12:27 +0200

    init commit

---
+++ src/main/java/com/boris/isa2022/Isa2022Application.java
@@ -0,0 +1,13 @@
+package com.boris.isa2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/boris/isa2022/Isa2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boris.isa2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isa2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
